f38860e2e80157b3a693483b1af7927daf1442fc
KarenKynnapas <karen.kynnapas@gmail.com>
2022-02-08 23:28:33 +0200

    Add modal

--- public/app.js
+++ public/app.js
@@ -1,10 +1,43 @@
 const nav = document.querySelector('.navbar');
-window.onscroll = function(){
+window.onscroll = function() {
     var top = window.scrollY;
     console.log(top);
-    if (top >= 100){
+    if (top >= 100) {
         nav.classList.add('active')
-    }else{
+    } else {
         nav.classList.remove('active');
     }
 }
+
+function addModalFunctionality(myModalId, myBtnId, spanId) {
+    // Get the modal
+    var modal = document.getElementById(myModalId);
+
+    // Get the button that opens the modal
+    var btn = document.getElementById(myBtnId);
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementById(spanId);
+
+    // When the user clicks the button, open the modal 
+    btn.onclick = function() {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function() {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function(event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    }
+}
+
+addModalFunctionality("modal1", "case1", "close1");
+addModalFunctionality("modal2", "case2", "close2");
+addModalFunctionality("modal3", "case3", "close3");
+addModalFunctionality("modal4", "case4", "close4");
